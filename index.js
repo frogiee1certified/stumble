e1d9af50974d7d3c342ab8d12f53fe33c819a18d
sky <skysth3limit@pm.me>
2024-06-13 02:36:11 -0400

    streamingassets fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function getStumble() {
 	let parsed_html = parse(stumble_html);
 	let parsed_json = JSON.parse(parsed_html.getElementById("__NEXT_DATA__").innerHTML).props.pageProps.gameConfig.attributes;
 	let base_url = `https://live-assets.web.stumbleguys.com/unity/${parsed_json.version}/${parsed_json.environment}/${parsed_json.hash}`;
-    let html = `<!DOCTYPE html><html><head><title>Stumble Guys</title><style>*{background:#000;width:100%;height:100%;overflow:visible;padding:0;margin:0;position:absolute;}</style></head><body><canvas id="uc"></canvas><script src="${base_url}/WebGL.loader.js"></script><script>createUnityInstance(document.querySelector("#uc"),{dataUrl:"${base_url}/WebGL.data.unityweb",frameworkUrl:"${base_url}/WebGL.framework.js.unityweb",codeUrl:"${base_url}/WebGL.wasm.unityweb"})</script></body></html>`;
+    let html = `<!DOCTYPE html><html><head><title>Stumble Guys</title><style>*{background:#000;width:100%;height:100%;overflow:visible;padding:0;margin:0;position:absolute;}</style></head><body><canvas id="uc"></canvas><script src="${base_url}/WebGL.loader.js"></script><script>createUnityInstance(document.querySelector("#uc"),{dataUrl:"${base_url}/WebGL.data.unityweb",frameworkUrl:"${base_url}/WebGL.framework.js.unityweb",codeUrl:"${base_url}/WebGL.wasm.unityweb",streamingAssetsUrl:"${base_url}/StreamingAssets"})</script></body></html>`;
 	fs.writeFileSync('./stumble.html', html);
 }
 getStumble()
